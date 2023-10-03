52b7a3128006c848eba9b738daccfe59ad7056f8
CodeLordGh <babohkossi228@icloud.com>
2023-10-03 16:45:01 +0000

    lock boxes fixes

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,27 +1,27 @@
-#!/usr/bin/pyton3
+#!/usr/bin/python3
+"""
+You have n number of locked boxes in front of you.
+Each box is numbered sequentially
+from 0 to n - 1 and each box may
+contain keys to the other boxes.
+"""
 
-"""
-You have n number of locked boxes in front of you. Each box is numbered
-sequentially from 0 to n - 1 and each box may contain keys to the other boxes.
-"""
 
 def canUnlockAll(boxes):
     """
-    Determines whether a series of locked boxes can be opened
-    based on keys that can be attained.
-    Solution to the lockboxes problem
+     a method that determines if all the boxes can be opened.
+
+    :param boxes:
+    :return: True or False
     """
-    if (type(boxes)) is not list:
-        return False
-    elif (len(boxes)) == 0:
+    if not boxes or type(boxes) is not list:
         return False
 
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False
-        for idx in range(len(boxes)):
-            boxes_checked = k in boxes[idx] and k != idx
-            if boxes_checked:
-                break
-        if boxes_checked is False:
-            return boxes_checked
-    return True
+    unlocked = [0]
+    for n in unlocked:
+        for key in boxes[n]:
+            if key not in unlocked and key < len(boxes):
+                unlocked.append(key)
+    if len(unlocked) == len(boxes):
+        return True
+    return False
