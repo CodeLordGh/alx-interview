c9bef421547c925199ab88c68c6831004aacdfcc
CodeLordGh <babohkossi228@icloud.com>
2023-11-25 15:21:11 +0000

    added documentaion for the module

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -1,5 +1,23 @@
 #!/usr/bin/python3
 
+"""
+Module for determining the minimum number of coins required to make a given change.
+
+This module provides a function `makeChange` that takes a list of coin denominations and a target change amount
+and returns the minimum number of coins needed to make the change. If the change cannot be made, it returns -1.
+
+Example Usage:
+
+```python
+import making_change
+
+coins = [1, 2, 25]
+total = 37
+min_coins = making_change.makeChange(coins, total)
+print(f"Minimum coins required: {min_coins}")
+
+"""
+
 # Function to make change with minimum number of coins
 def makeChange(coins, total):
     # If total is 0 or less, return 0
@@ -27,4 +45,3 @@ def makeChange(coins, total):
         return -1
 
     return minCoins[total]
-
